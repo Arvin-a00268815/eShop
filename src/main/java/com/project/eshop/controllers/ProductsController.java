80d8f5e52e2b478aa17ef2c72bca21762b478d31
a00268815-arvin <a00268815@student.ait.ie>
2020-03-17 01:05:58 +0000

    update item

--- src/main/java/com/project/eshop/controllers/ProductsController.java
+++ src/main/java/com/project/eshop/controllers/ProductsController.java
@@ -2,9 +2,14 @@ package com.project.eshop.controllers;
 
 import com.project.eshop.model.Item;
 import com.project.eshop.repository.ItemsRepository;
+import com.sun.jndi.toolkit.url.Uri;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,4 +40,22 @@ public class ProductsController {
         return repository.findById(id);
     }
 
+    @DeleteMapping("/removeItem/{id}")
+    public void removeItem(@PathVariable int id){
+        repository.deleteById(id);
+    }
+
+    @PutMapping("/updateItem")
+    public ResponseEntity updateItem(@RequestBody Item item){
+        if (repository.existsById(item.getId())) {
+            repository.save(item);
+            return ResponseEntity.status(HttpStatus.OK).build();
+        }else{
+            repository.save(item);
+
+            URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}")
+                    .buildAndExpand(item.getId()).toUri();
+            return ResponseEntity.created(location).build();
+        }
+    }
 }
